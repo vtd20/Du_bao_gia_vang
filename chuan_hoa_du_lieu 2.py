62c0902fe58f342d6969b30f5630c7591aab9a3c
tabang205 <84980304+tabang205@users.noreply.github.com>
2025-05-11 14:07:37 +0700

    Update chuan_hoa_du_lieu 2.py

--- chuan_hoa_du_lieu 2.py
+++ chuan_hoa_du_lieu 2.py
@@ -5,18 +5,18 @@ from pyspark.sql.functions import udf, col
 import numpy as np
 
 def scale_and_prepare_data(df, test_size, window_size=30):
-    # Define UDF to convert double to vector
+    # Định nghĩa UDF để chuyển đổi kiểu double thành vector
     to_vector_udf = udf(lambda x: Vectors.dense([x]), VectorUDT())
     
-    # Prepare data for scaling
+    # Chuẩn bị dữ liệu để chuẩn hóa
     price_df = df.withColumn("features", to_vector_udf("Price"))
     
-    # Initialize and fit MinMaxScaler
+    # Khởi tạo và huấn luyện MinMaxScaler
     scaler = MinMaxScaler(inputCol="features", outputCol="scaled_features")
     scaler_model = scaler.fit(price_df)
     scaled_df = scaler_model.transform(price_df)
     
-    # Training set
+    # Tập huấn luyện
     train_data_df = df.filter(col("index") <= df.count() - test_size).select("Price", "index", "Date")
     train_data_df = train_data_df.withColumn("features", to_vector_udf("Price"))
     train_data_scaled_df = scaler_model.transform(train_data_df)
@@ -27,7 +27,7 @@ def scale_and_prepare_data(df, test_size, window_size=30):
     X_train = np.array(X_train)
     y_train = np.array(y_train)
     
-    # Test set
+    # Tập kiểm tra
     test_data_df = df.filter(col("index") > df.count() - test_size - window_size).select("Price", "index", "Date")
     test_data_df = test_data_df.withColumn("features", to_vector_udf("Price"))
     test_data_scaled_df = scaler_model.transform(test_data_df)
@@ -38,15 +38,15 @@ def scale_and_prepare_data(df, test_size, window_size=30):
     X_test = np.array(X_test)
     y_test = np.array(y_test)
     
-    # Reshape for LSTM
+    # Định hình lại cho mô hình LSTM
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
     X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
     y_train = np.reshape(y_train, (-1, 1))
     y_test = np.reshape(y_test, (-1, 1))
     
-    print('X_train Shape: ', X_train.shape)
-    print('y_train Shape: ', y_train.shape)
-    print('X_test Shape:  ', X_test.shape)
-    print('y_test Shape:  ', y_test.shape)
+    print('Hình dạng X_train: ', X_train.shape)
+    print('Hình dạng y_train: ', y_train.shape)
+    print('Hình dạng X_test:  ', X_test.shape)
+    print('Hình dạng y_test:  ', y_test.shape)
     
     return scaler_model, X_train, y_train, X_test, y_test, train_data
